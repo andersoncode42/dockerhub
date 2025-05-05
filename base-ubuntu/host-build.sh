99c0807eccce565e275699e254f8467a4b9e7932
Anderson <andersonsergipano@gmail.com>
2025-05-05 00:54:02 -0300

    Alterado o comando podman pelo comando docker

--- base-ubuntu/host-build.sh
+++ base-ubuntu/host-build.sh
@@ -3,5 +3,5 @@ set -e
 imagem=$(basename "$PWD")
 
 # Elimina os containers para poder reconstruir a imagem
-podman container rm --force imagem
-podman build --no-cache --rm -t "$imagem":latest .
+docker container rm --force imagem
+docker build --no-cache --rm -t "$imagem":latest .
